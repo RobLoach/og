df93063ae5fdfaffc3b5c5355674e29e5858fa78
moshe weitzman <weitzman@cyrve.com>
2006-01-04 23:03:33 +0000

    adding 4.7 branch. i though i had done that already but apparently not.

---
+++ node-og.tpl.php
@@ -0,0 +1,13 @@
+<?php //$Id$ 
+?>
+<div class="node<?php print ($sticky) ? " sticky" : ""; ?>">
+  <?php if ($page == 0): ?>
+    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  <?php endif; ?>
+
+
+  <div class="content">
+    <?php print $content ?>
+  </div>
+
+</div>
