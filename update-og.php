9da4f5b5352006cfc53f1095d9138647421b4146
moshe weitzman <weitzman@cyrve.com>
2006-02-11 03:07:10 +0000

    minor improvements

--- update-og.php
+++ update-og.php
@@ -19,7 +19,7 @@ while ($object = db_fetch_object($result)) {
 $sql = "SELECT nid FROM {node} WHERE type = 'og'";
 $result = db_query($sql);
 while ($row = db_fetch_object($result)) {
-  $sql = "REPLACE INTO {node_access} (nid, gid, realm, grant_view, grant_update, grant_delete) VALUES (%d, %d, 'og_group', 1, 1, 1)";
+  $sql = "REPLACE INTO {node_access} (nid, gid, realm, grant_view, grant_update, grant_delete) VALUES (%d, %d, 'og_group', 1, 1, 0)";
   db_query($sql, $row->nid, $row->nid);
 }
 
