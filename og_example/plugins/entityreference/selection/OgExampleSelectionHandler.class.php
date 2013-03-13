54cc05722a9f08f54b6cca14a1f4c205a7c2dc99
Amitai Burstein <amitai@gizra.com>
2013-03-13 21:55:22 +0200

    More work.

--- og_example/plugins/entityreference/selection/OgExampleSelectionHandler.class.php
+++ og_example/plugins/entityreference/selection/OgExampleSelectionHandler.class.php
@@ -9,21 +9,29 @@
 class OgExampleSelectionHandler extends OgSelectionHandler {
 
   /**
+   * Implements EntityReferenceHandler::getInstance().
+   */
+  public static function getInstance($field, $instance = NULL, $entity_type = NULL, $entity = NULL) {
+    return new OgExampleSelectionHandler($field, $instance, $entity_type, $entity);
+  }
+
+  /**
    * Build an EntityFieldQuery to get referencable entities.
    */
   public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
-    $group_type = $this->field['settings']['target_type'];
-    $field_mode = $this->instance['field_mode'];
-    // Show the user only the groups they belong to.
-    if ($field_mode == 'default' && $group_type == 'node') {
-      $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
-      $query = $handler->buildEntityFieldQuery($match, $match_operator);
-      $query->propertyCondition('title', 'a', 'CONTAINS');
-    }
-    else {
-      $query = parent::buildEntityFieldQuery($match, $match_operator);
+    if (!empty($this->instance['field_mode'])) {
+      $group_type = $this->field['settings']['target_type'];
+      $field_mode = $this->instance['field_mode'];
+      // Show the user only the groups they belong to.
+      if ($field_mode == 'default' && $group_type == 'node') {
+        $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
+        $query = $handler->buildEntityFieldQuery($match, $match_operator);
+        $query->propertyCondition('nid', '5', '>');
+      }
+      return $query;
     }
 
+    $query = parent::buildEntityFieldQuery($match, $match_operator);
     return $query;
   }
 }
