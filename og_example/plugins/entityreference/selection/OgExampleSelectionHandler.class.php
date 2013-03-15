1b55e5035471ac8e467e7411518351b2b2a56f62
Amitai Burstein <amitai@gizra.com>
2013-03-15 11:33:40 +0200

    Add docs.

--- og_example/plugins/entityreference/selection/OgExampleSelectionHandler.class.php
+++ og_example/plugins/entityreference/selection/OgExampleSelectionHandler.class.php
@@ -9,14 +9,19 @@
 class OgExampleSelectionHandler extends OgSelectionHandler {
 
   /**
-   * Implements EntityReferenceHandler::getInstance().
+   * Overrides OgSelectionHandler::getInstance().
    */
   public static function getInstance($field, $instance = NULL, $entity_type = NULL, $entity = NULL) {
     return new OgExampleSelectionHandler($field, $instance, $entity_type, $entity);
   }
 
   /**
-   * Build an EntityFieldQuery to get referencable entities.
+   * Overrides OgSelectionHandler::buildEntityFieldQuery().
+   *
+   * This is an example of "subgorups" (but without getting into the logic of subgrouping).
+   * The idea here is to show we can set "My groups" and "Other groups" to reference different groups by different
+   * logic. In this example, all group nodes below node ID 5, will appear under "My groups", and the rest will appear
+   * under "Other groups", for administrators.
    */
   public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
     $group_type = $this->field['settings']['target_type'];
@@ -27,9 +32,11 @@ class OgExampleSelectionHandler extends OgSelectionHandler {
     }
 
     $field_mode = $this->instance['field_mode'];
-    // Show the user only the groups they belong to.
     $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
     $query = $handler->buildEntityFieldQuery($match, $match_operator);
+
+    // Show only the entities that are active groups.
+    $query->fieldCondition(OG_GROUP_FIELD, 'value', 1, '=');
 
     if ($field_mode == 'default') {
       $query->propertyCondition('nid', '5', '<=');
