4878bffa48c448c40e34879db954e0c835273440
Amitai Burstein <amitai@gizra.com>
2013-03-15 11:30:08 +0200

    Fix subgroups.

--- og_example/plugins/entityreference/selection/OgExampleSelectionHandler.class.php
+++ og_example/plugins/entityreference/selection/OgExampleSelectionHandler.class.php
@@ -19,19 +19,25 @@ class OgExampleSelectionHandler extends OgSelectionHandler {
    * Build an EntityFieldQuery to get referencable entities.
    */
   public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
-    if (!empty($this->instance['field_mode'])) {
-      $group_type = $this->field['settings']['target_type'];
-      $field_mode = $this->instance['field_mode'];
-      // Show the user only the groups they belong to.
-      if ($field_mode == 'default' && $group_type == 'node') {
-        $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
-        $query = $handler->buildEntityFieldQuery($match, $match_operator);
-        $query->propertyCondition('nid', '5', '>');
-      }
-      return $query;
+    $group_type = $this->field['settings']['target_type'];
+
+
+    if (empty($this->instance['field_mode']) || $group_type != 'node') {
+      return parent::buildEntityFieldQuery($match, $match_operator);
     }
 
-    $query = parent::buildEntityFieldQuery($match, $match_operator);
+    $field_mode = $this->instance['field_mode'];
+    // Show the user only the groups they belong to.
+    $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
+    $query = $handler->buildEntityFieldQuery($match, $match_operator);
+
+    if ($field_mode == 'default') {
+      $query->propertyCondition('nid', '5', '<=');
+    }
+    else {
+      $query->propertyCondition('nid', '5', '>');
+    }
+
     return $query;
   }
 }
